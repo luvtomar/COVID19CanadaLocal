48c865a13bd6ef82cb268f8aa7be68bc68ea6d9b
Luv Tomar <luvtomar1234@gmail.com>
2020-04-26 12:19:27 -0400

    4th try

--- COVID19CanadaLocal/settings.py
+++ COVID19CanadaLocal/settings.py
@@ -133,11 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = ''
+
 STATIC_URL = '/static/'
 
-# Add these new lines
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = ( os.path.join('static'), )
